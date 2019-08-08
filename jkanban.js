15681b2ff7932496846e58051a0bd7b182ba0aa1
Julian Hildemann <julian_ios@Hildemann.eu>
2019-08-08 11:32:24 +0200

    typo

--- jkanban.js
+++ jkanban.js
@@ -183,7 +183,8 @@ var dragula = require("dragula");
       allClasses.map(function(value) {
         nodeItem.classList.add(value);
       });
-	  	  
+	  
+	  
       //console.log(element);
       if (typeof element.id !== "undefined" && element.id !== "") {
         nodeItem.setAttribute("data-eid", element.id);
@@ -231,7 +232,7 @@ var dragula = require("dragula");
 			  
 			  self.container.style.width = 	Math.round((newBoardWidth+2*parseInt(self.options.gutter))*els.length)+"px";	
 			  console.log("board width: "+(newBoardWidth+2*parseInt(self.options.gutter))*els.length);
-			}elsec
+			}else{
 				
   	  		  [].forEach.call(els, function (el) {
   	  			  el.style.width = parseInt(self.options.widthBoard) + "px";
